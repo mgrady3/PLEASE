7f34fe2bcf07ca85b034d6dcf528ea41b1b91dec
mgrady <max.grady@gmail.com>
2021-05-17 23:57:17 -0500

    DEV: implement model interfaces
    This commit introduces two interface classes to the please.model
    sub-package, which define the core data models for electron
    microscope images and electron microscope experiments.
    
    Additionally, a constants module is added to the model sub-package
    that contains information used by the models to validate data for
    things like data type, experiment type, file formats, etc...

---
+++ please/model/constants.py
@@ -0,0 +1,30 @@
+""" This module contains constants used by various model code
+"""
+
+# TODO: add support for ARPES data and potentially other types
+# See GH Issue: #11
+SUPPORTED_EXPERIMENT_TYPES = {
+    'LEEM',
+    'LEED',
+}
+
+SUPPORTED_DATA_TYPES = {
+    'Image',
+    'Raw',
+}
+
+SUPPORTED_IMAGE_FORMATS = {
+    'PNG',
+    'TIFF',
+}
+
+ENDIAN_STRINGS = {
+    'L',  # Little-endian data
+    'M',  # Big-endian data
+}
+
+EXTERNAL_PARAMETERS = {
+    'Energy',
+    'Temperature',
+    'Time'
+}
